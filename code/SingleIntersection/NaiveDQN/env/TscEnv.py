870192edd5f08baf0254e26ad97bc4bf17998fd2
bisonliao <11228490@qq.com>
2025-09-24 16:55:24 +0800

    x

---
+++ code/SingleIntersection/NaiveDQN/env/TscEnv.py
@@ -0,0 +1,198 @@
+import datetime
+import random
+import time
+
+import numpy as np
+import torch
+import gymnasium as gym
+import numpy as np
+from gymnasium import spaces
+from torch.utils.tensorboard import SummaryWriter
+import math
+import cityflow
+from collections import deque
+from conf.simple_config import Config
+
+
+
+class TscEnv(gym.Env):
+   
+    def __init__(self, writer:SummaryWriter):
+
+        super(TscEnv, self).__init__()
+        self.writer = writer
+
+        self.eng = cityflow.Engine(f"{Config.ENV_BASE_DIR}/config.json", thread_num=4)
+        
+        self.vehicles_ever_seen = set() # 记录凡是出现过的车辆
+       
+        # 定义动作空间和状态空间
+        self.action_space = spaces.Discrete(4)
+
+        self.observation_space = spaces.Box(
+            low= np.array([0,0,0,0,  0,  0,0,0,0,  0,0,0,0, 0,0,0,0]),
+            high=np.array([1,1,1,1,  1,  1,1,1,1,  1,1,1,1, 1,1,1,1]),
+            dtype=np.float32
+        )
+        
+
+        # 环境参数
+        self.max_steps = 2000  # 最大步数
+        self.current_step = 0 #每一回合里的步数计数器
+        self.total_step = 0  #环境运行过程中一直累加的总的计数器
+        self.durations = deque(maxlen=100) #统计过去100个相位的时间长度
+
+        self.reset()
+
+    
+
+    def reset(self, seed=None, save_replay=False):
+        """重置环境到初始状态"""
+        #self.eng.set_save_replay(save_replay)
+        #self.eng.set_replay_file(f'replay_{datetime.datetime.now().strftime("%y%m%d_%H%M%S")}.txt')
+        self.eng.reset()
+        self.eng.set_tl_phase('intersection_1_1', 1)
+        self.current_phase = 1
+
+        self.current_step = 0
+        self.phase_duration = 0 # 一个相位持续多久
+        self.vehicles_ever_seen.clear()
+        if len(self.durations) > 0:
+            self.writer.add_scalar('phase_duration/max', np.max(self.durations), self.total_step)
+            self.writer.add_scalar('phase_duration/min', np.min(self.durations), self.total_step)
+            self.writer.add_scalar('phase_duration/avg', np.mean(self.durations), self.total_step)
+            self.writer.add_scalar('phase_duration/std', np.std(self.durations), self.total_step)
+        self.durations.clear()
+        
+        # 获取初始状态
+        state = self._get_state()
+
+        return state,{}
+
+    def step(self, action):
+
+        new_phase = action + 1
+
+        if self.current_phase != new_phase: #相位有变化
+            # 先来3s红灯清场
+            self.eng.set_tl_phase('intersection_1_1', 0)
+            for _ in range(3):
+                self.eng.next_step()
+            # 然后轮转到下一个方向放行
+            self.eng.set_tl_phase('intersection_1_1',new_phase)
+            self.current_phase = new_phase
+
+            self.durations.append(self.phase_duration)
+            self.phase_duration = 1
+        else:
+            self.phase_duration += 1
+
+        
+        self.eng.next_step()
+
+        self.current_step += 1
+        self.total_step += 1
+
+        
+
+        # 获取新状态
+        state = self._get_state()
+
+        # 计算奖励
+        reward = 0
+        terminated = False
+
+
+        # 检查是否超过最大步数
+        truncated = False
+        if self.current_step >= self.max_steps:
+            truncated = True
+
+
+        info = {
+            "steps": self.current_step,
+        }
+
+
+        return state, reward, terminated, truncated, info
+    
+    def _route2phaseID(self, route:str):
+        route = route.strip()
+        if route == "road_2_1_2 road_1_1_2" or route == "road_0_1_0 road_1_1_0": # 东西直行
+            return 0
+        if route == "road_1_2_3 road_1_1_3" or route == "road_1_0_1 road_1_1_1": #南北直行
+            return 1
+        if route == "road_2_1_2 road_1_1_3" or route == "road_0_1_0 road_1_1_1": # 东西左转
+            return 2
+        if route == "road_1_2_3 road_1_1_0" or route == "road_1_0_1 road_1_1_2": # 南北左转
+            return 3
+        assert False, f"invalid route {route} "
+
+    def calc_vehicle_info(self):
+        
+        vehicles_present = set()
+
+        self.vehicle_dense = np.zeros(Config.PHASE_NUM, dtype=np.float32) #停止线前面30m范围内的车辆密度
+        self.avg_speed = np.zeros(Config.PHASE_NUM, dtype=np.float32)#停止线前面30m范围内的车辆平均速度
+        self.waiting_qlen = np.zeros(Config.PHASE_NUM, dtype=np.float32) # 入口车道上排队的车辆数目，由于每个方向都是一个入口车道，所以这里就是等待队列的长度
+
+
+        speed_list = [deque(maxlen=100) for _ in range(Config.PHASE_NUM)]
+        vehicle_ids = self.eng.get_vehicles(True)
+        for vid in vehicle_ids:
+            try:
+                info = self.eng.get_vehicle_info(vid)
+                if info.get('running', "0") != "1": #whether the vehicle is running
+                    continue
+
+                #  The next intersection if the vehicle is running on a lane
+                if info.get('intersection', "")!= 'intersection_1_1':
+                    continue
+
+                self.vehicles_ever_seen.add(vid)
+                vehicles_present.add(vid)
+                
+
+                speed = info['speed']       
+                route = info['route'] # A string contains ids of following roads in the vehicle’s route which are separated by space
+                phase_id = self._route2phaseID(route)
+
+                if float(speed) < 0.1:
+                    self.waiting_qlen[phase_id] += 1
+
+                drivable = info['drivable']
+                vehicle_distance = info["distance"]  # 已行驶距离
+                to_stop_line = Config.LANE_LEN - float(vehicle_distance)
+                if to_stop_line > (Config.CELL_LEN *Config.OBSERV_CELL_NUM) or to_stop_line < 0:
+                    continue
+
+                
+                self.vehicle_dense[phase_id] += 1.0 / (Config.OBSERV_CELL_NUM * 2) 
+                speed_list[phase_id].append( float(speed) )
+
+            except Exception as e:
+                print(f"Error retrieving info for vehicle {vid}: {e}")
+        
+        for i in range(Config.PHASE_NUM):
+            if len(speed_list[i]) > 0:
+                self.avg_speed[i] = sum(speed_list[i]) / len(speed_list[i])
+
+        self.throughput = len(self.vehicles_ever_seen - vehicles_present) #从回合开始（reset后）到目前，通过该路口的车辆数，即吞吐量
+
+    def _get_state(self):
+        return None
+
+    
+    def get_biz_metrics(self): 
+        att = self.eng.get_average_travel_time()
+        thput =self.throughput
+        return att, thput
+
+
+    def render(self, mode='human'):
+        """渲染环境"""
+        pass  # PyBullet会自动处理渲染
+
+    def close(self):
+        """关闭环境"""
+        pass
---
+++ code/SingleIntersection/NaiveDQN/simple_agent.py
@@ -0,0 +1,243 @@
+
+import random
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import numpy as np
+from collections import deque
+import datetime
+import os
+from env.TscEnv import TscEnv
+from torch.utils.tensorboard import SummaryWriter
+from conf.simple_config import Config
+from env.TscEnv import TscEnv
+from model.simple_model import DQN
+
+
+'''
+特征里，每个方向上的排队长度要安排上
+出现了没有车通过，也一直亮着绿灯的情况。所以最大相位时长要做限制。最小的也有必要，虽然学习到了较长的相位，但还是有个别相位时间很短
+某个队列长度超过阈值，就终止回合，并做较大的处罚
+
+有的方向上车辆不多，就会一直等一直等。解决方法：各个方向上的排队最大时长要输入，并针对多次达到最大阈值，给与较大的处罚；或者奖励除了是等待车辆数的负数，也是等待时长的负数？两者的数量级要对等才行
+'''
+
+
+class SimpleAgent:
+    def __init__(self):
+        
+        # 设备选择
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.writer = SummaryWriter(f'logs/tsc_{datetime.datetime.now().strftime("%y%m%d_%H%M%S")}')
+        # 超参数
+        self.gamma = 0.9  # 折扣因子
+        self.epsilon = 1.0  # 初始探索率
+        self.epsilon_min = 0.01  # 最低探索率
+        self.epsilon_decay = 0.97  # 探索率衰减
+        self.learning_rate = 1e-3  # 学习率
+        self.batch_size = 128  # 经验回放的批量大小
+        self.memory_size = 100000  # 经验池大小
+        self.target_update_freq = 3  # 目标网络更新频率
+
+        self.env = TscEnv(self.writer)
+        self.n_state = self.env.observation_space.shape[0]  # 状态维度
+        self.n_action = self.env.action_space.n  # 动作数量
+        print(f'action number={self.n_action}, obs shape={self.env.observation_space.shape}')
+
+        # 初始化网络
+        self.model = DQN(self.n_state, self.n_action).to(self.device)
+        self.target_model = DQN(self.n_state, self.n_action).to(self.device)
+        self.target_model.load_state_dict(self.model.state_dict())
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        self.memory = deque(maxlen=self.memory_size)
+
+        self.long_q_duration = 0
+
+    def reset(self):
+        self.long_q_duration = 0
+
+
+    def select_action(self, state, epsilon):
+        """基于 ε-greedy 选择动作"""
+        if random.random() < epsilon:
+            return random.randint(0, self.n_action - 1)  # 随机选择
+        else:
+            state = torch.FloatTensor(state).unsqueeze(0).to(self.device)  # 变换前：[4] -> 变换后：[1, 4]
+            return self.model(state).argmax(1).item()  # 选取 Q 值最大的动作
+
+
+    def train(self):
+        if len(self.memory) < self.batch_size:
+            return 9999.0  # 经验池数据不足时不训练
+
+        batch = random.sample(self.memory, self.batch_size)
+        states, actions, rewards, next_states, dones = zip(*batch)
+
+        states = torch.FloatTensor(states).to(self.device)  # (batch_size, 4)
+        actions = torch.LongTensor(actions).unsqueeze(1).to(self.device)  # (batch_size,) -> (batch_size, 1)
+        rewards = torch.FloatTensor(rewards).unsqueeze(1).to(self.device)  # (batch_size,) -> (batch_size, 1)
+        next_states = torch.FloatTensor(next_states).to(self.device)  # (batch_size, 4)
+        dones = torch.FloatTensor(dones).unsqueeze(1).to(self.device)  # (batch_size,) -> (batch_size, 1)
+
+        # 计算当前 Q 值
+        q_values = self.model(states).gather(1, actions)  # 从 Q(s, a) 选取执行的动作 Q 值
+
+        # 计算目标 Q 值
+        next_q_values = self.target_model(next_states).max(1, keepdim=True)[0]  # 选取 Q(s', a') 的最大值
+        target_q_values = rewards + self.gamma * next_q_values * (1 - dones)  # TD 目标
+
+        # 计算损失
+        loss = F.mse_loss(q_values, target_q_values.detach())
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        return loss.item()
+
+
+    def save_checkpoint(self, id):
+        os.makedirs('./checkpoints/', exist_ok=True)
+        path=f"./checkpoints/dqn_checkpoint_{id}.pth"
+        torch.save(self.model.state_dict(), path)
+        print(f"Checkpoint saved to {path}")
+
+
+    def load_checkpoint(self, path):
+        if os.path.exists(path):
+            self.model.load_state_dict(torch.load(path, map_location=self.device))
+            print(f"Checkpoint loaded from {path}")
+        else:
+            print("No checkpoint found, starting from scratch.")
+
+    def observation_process(self):
+        self.env.calc_vehicle_info()
+
+        # 当前相位的one-hot表示
+        phase_onehot = np.zeros(Config.PHASE_NUM, dtype=np.long)
+        phase_onehot[self.env.current_phase-1] = 1
+               
+
+        # 合并所有状态信息
+        state = np.concatenate(
+            [
+            phase_onehot,
+            [self.env.phase_duration / Config.MAX_PHASE_DUR],
+            self.env.vehicle_dense, 
+            self.env.avg_speed / 22,
+            self.env.waiting_qlen / Config.MAX_QLEN,
+     
+            ], 
+            dtype=np.float32)
+
+        return state
+    def reward_shape(self):
+
+        # 某些方向的等待车辆堆积超过阈值,
+        if np.any(self.env.waiting_qlen >= Config.MAX_QLEN):
+            self.long_q_duration += 1
+        else:
+            self.long_q_duration = 0
+
+        if self.long_q_duration > 20:
+            #print(f'too long queue')
+            return -10
+        
+        # 某个相位持续时间超过 30s
+        if self.env.phase_duration >= Config.MAX_PHASE_DUR:  
+            #print(f'too long phase')
+            return -10
+        
+
+        vehicle_cnt = np.sum(self.env.waiting_qlen)
+
+        if self.env.phase_duration == 1: #刚经历了切换，有3s红灯时间成本，所以-1处罚
+            reward = -(vehicle_cnt**0.25) - 1
+        else:
+            reward = -(vehicle_cnt**0.25)
+
+        return reward
+
+
+    def learn_or_eval(self, mode):
+        
+        if mode == "train":
+            episodes = 1000
+
+            '''if input("preload model?[Y/n]") == "Y":
+                self.load_checkpoint('./checkpoints/dqn_checkpoint_-20657.283203125.pth')
+                self.epsilon = 0.1
+                print(f'preload successfully')'''
+
+            for episode in range(episodes):
+                save_replay = ( episode > 20 and episode % 37 == 7 )
+
+                self.reset()
+                self.env.reset(save_replay=save_replay)
+                state = self.observation_process()
+                total_reward = 0
+
+                while True:
+                    action = self.select_action(state, self.epsilon)
+                    _, _, terminated, truncated, _ = self.env.step(action)
+                    next_state = self.observation_process()
+                    reward = self.reward_shape()
+                    done = terminated or truncated
+                    if self.env.total_step % 117 == 7:
+                        self.writer.add_scalar('train/step_reward', reward, self.env.total_step)
+
+                    
+                    # 经验回放缓存
+                    self.memory.append((state, action, reward, next_state, done))
+                    state = next_state
+                    total_reward += reward
+
+                    # 训练 DQN
+                    loss = self.train()
+
+                    if done:
+                        break
+
+                # 逐步降低 epsilon，减少随机探索，提高利用率
+                self.epsilon = max(self.epsilon_min, self.epsilon * self.epsilon_decay)
+
+                # 定期更新目标网络，提高稳定性
+                if episode % self.target_update_freq == 0:
+                    self.target_model.load_state_dict(self.model.state_dict())
+
+                # 定期保存模型
+                if episode % 25 == 1:
+                    self.save_checkpoint(f'{episode}_{total_reward}')
+
+                att,thrput = self.env.get_biz_metrics()
+
+                print(f"{datetime.datetime.now().strftime('%H:%M:%S')} Episode {episode}, Reward: {total_reward:.2f}, Epsilon: {self.epsilon:.3f}, loss:{loss:.8f}")
+                self.writer.add_scalar('train/episode_reward', total_reward, episode)
+                self.writer.add_scalar('train/epsilon', self.epsilon, episode)
+                self.writer.add_scalar('train/loss', loss, episode)
+                self.writer.add_scalar('train/att', att, episode)
+                self.writer.add_scalar('train/thrput', thrput, episode)
+        else:
+            self.load_checkpoint('./checkpoints/dqn_checkpoint_149_-5373.30029296875.pth')
+            self.model.eval()
+            eval_env = TscEnv(self.writer)
+            self.env = eval_env
+
+            eval_env.reset(save_replay=True)
+            state = self.observation_process()
+
+            while True:
+                action = self.select_action(state, 0)
+                _, _, terminated, truncated, _ = eval_env.step(action)
+                next_state = self.observation_process()
+                done = terminated or truncated
+                state = next_state
+                    
+                if done:
+                    break
+
+if __name__ == "__main__":
+    agent = SimpleAgent()
+    agent.learn_or_eval('train')
+    
+    
